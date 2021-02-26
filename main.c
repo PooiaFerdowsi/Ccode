f85f87d72546ce10076e396e4397248906ba537c
PooiaFerdowsi <76428946+PooiaFerdowsi@users.noreply.github.com>
2021-02-26 18:01:03 +0330

    Update main.c

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include "include.h"
 #include <stdio.h>
+extern Pooia;
 int main(){
   int *ptr;
   printf("HelloWorld");
@@ -8,7 +9,11 @@ int main(){
   int *ptr0;
   int **dptr0;
   static char[] = "text";
-  }
+  } pl1;
   register inCpu;
+  auto int my;
+  sec1:
+    pl1->myType varn = 100UL;
+  goto sec1;
   // notes 
 }
